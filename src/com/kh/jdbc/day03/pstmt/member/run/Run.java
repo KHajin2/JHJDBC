b3f436d7f464bc15ae8e114d8981453559c026a6
user1 <user1@DESKTOP-Q2CDVLH>
2025-08-26 17:46:14 +0900

    [ADD] 회원관리프로그램 PreparedStatement, Service, Singleton 적용 1. 입력값을 검증하여 SqlInjection 방어할 수 있는 PreparedStatement 적용하여 JDBC 프로그래밍 완료 2. DAO의 역할을 분할하기 위해서 Service 클래스 생성, 서비스 클래스에서는 DBMS연결 생성하여 DAO에 넘겨줌 - DAO는 코드가 줄어들게 됨 3. 연결을 만들어주는 코드를 JDBCTemplate 클래스에 이동 후 JDBCTemplate에는 Singleton 패턴을 적용하여 생성된 객체를 재사용하게 함. 4. rsetToMember() 메소드를 이용해서 후처리 하는 부분을 재사용함.

---
+++ src/com/kh/jdbc/day03/pstmt/member/run/Run.java
@@ -0,0 +1,10 @@
+package com.kh.jdbc.day03.pstmt.member.run;
+
+import com.kh.jdbc.day03.pstmt.member.view.MemberView;
+
+public class Run {
+	public static void main(String[] args) {
+		MemberView view = new MemberView();
+		view.startProgram();
+	}
+}
